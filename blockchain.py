9b383db0b7cd5bf24e0b959ebe4a76b5b42382de
Trevahok <vighneshss@gmail.com>
2018-10-08 16:13:51 +0530

    make skeleton blockchain for crypto

--- blockchain.py
+++ blockchain.py
@@ -16,7 +16,10 @@ class Blockchain():
             'proof' : proof, 
             'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
-        pass
+        self.current_transacitons=[]
+        self.chain.append(block)
+
+        return block
     def new_transaction(self, sender , recipient , amount):
         self.current_transaciton.append(
             {
@@ -26,9 +29,23 @@ class Blockchain():
             }
         )
         return self.last_block['index']+1
+
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block , sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
     @property
     def last_block(self):
         return self.chain[-1] 
+    def proof_of_work(self , last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof+=1
+        return proof
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4]= '0000'
+
