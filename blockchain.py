2db893757f1ed21be3d69b1e0cc468e7b71087e8
Trevahok <vighneshss@gmail.com>
2018-10-08 16:31:57 +0530

    add basic flask for api

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,7 @@
 import hashlib, json 
+from textwrap import dedent
+from uuid import uuid4
+from flask import Flask 
 from time import time
 class Blockchain():
     def __init__(self, *args, **kwargs):
@@ -47,5 +50,29 @@ class Blockchain():
     def valid_proof(last_proof, proof):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4]= '0000'
+        return guess_hash[:4]== '0000'
 
+
+
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods= ['GET'])
+def mine():
+    return 'bitch money here'
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return ' new shit here'
+@app.route('/chain' , methods=['GET'])
+def full_chain():
+    response={
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+
+    return json.dumps(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
