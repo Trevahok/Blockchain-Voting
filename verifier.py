9d2afb60779a47da0361cecb377f552ddb0776ac
Trevahok <vighneshss@gmail.com>
2018-10-10 20:33:22 +0530

    add party verification

--- verifier.py
+++ verifier.py
@@ -1,15 +1,24 @@
 from flask import Flask,jsonify
 from json import load
+
 with open('valid_aids_file.json', 'r') as aids_file:
     valid_aids = load(aids_file)
+with open('valid_parties.json', 'r') as parties_file:
+    valid_parties= load(parties_file)
 app = Flask(__name__)
-@app.route('/verify/<int:aid>', methods= ['GET'])
+@app.route('/aids/<int:aid>', methods= ['GET'])
 def verify_aadhar(aid):
     return jsonify(
         {
             'valid': aid in valid_aids['aids'],   
         }
     )
-
+@app.route('/party/<party>', methods= ['GET'])
+def verify_party(party):
+    return jsonify(
+        {
+            'valid': party in valid_parties['parties'],   
+        }
+    )
 if __name__ == '__main__':
     app.run(debug=True)
