87b11609a090454ea212d7bcabea9ccef5713fa6
Trevahok <vighneshss@gmail.com>
2018-10-13 20:08:42 +0530

    remove useless files

--- client.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('client.html')
-
-app.run(debug=True,port=8000,host='localhost')
